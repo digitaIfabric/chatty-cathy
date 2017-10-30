01a1b4bb30761057c47bbee5d380016aeeea5773
digitalfabric92 <dwawryko@gmail.com>
2017-10-30 18:40:11 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
+    console.log("Rendering <App/>")
     return (
-      <h1>Hello React :)</h1>
+      <div>
+       <nav className="navbar">
+              <a href="/" className="navbar-brand">Chatty-Cathy</a>
+            </nav>
+          <Message />
+          <MessageList />
+          <ChatBar />
+      </div>
     );
   }
 }
