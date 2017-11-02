82e0ec831707cd5884236a78870a54b351f2e79c
digitalfabric92 <dwawryko@gmail.com>
2017-11-02 18:43:56 -0400

    Hardcoded ip address

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.socket = new WebSocket('ws://localhost:3001');
+    this.socket = new WebSocket('ws://172.20.10.4:3001');
     console.log('Connected to WebSocket 1st log');
 
     this.socket.onopen = (e) => {
@@ -70,6 +70,8 @@ class App extends Component {
       const newChatBarMessage = {type: 'postMessage', username: this.state.currentUser.name, content: e.target.value}
       console.log("KeyDown event tracked");
       this.state.socket.send(JSON.stringify(newChatBarMessage));
+      //
+
       // socket.io 90%
       // Send message to this.state Message
       // Reset the value to an empty string for next message
@@ -94,83 +96,3 @@ class App extends Component {
   }
 }
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {Component} from 'react';
-// import MessageList from './MessageList.jsx';
-// import ChatBar from './ChatBar.jsx'
-// import NavBar from './NavBar.jsx'
-//
-// class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       currentUser: {name: "Anonymous"}, // optional. if currentUser is not defined, it means the user is Anonymous
-//       messages: []
-//     }
-//   }
-//
-//
-//   componentDidMount() {
-//     this.socket = new WebSocket('ws://localhost:3001')
-//     this.socket.onopen = (e) => {
-//       console.log('connected to websocket')
-//     }
-//     this.socket.onmessage = (e) => {
-//       const messages = this.state.messages.concat(JSON.parse(event.data))
-//       this.setState({messages: messages})
-//     }
-//   }
-//
-//   sendMessage(msg) {
-//     this.socket.send(JSON.stringify(msg))
-//   }
-//
-//   changeUser(e) {
-//     if(e.key == "Enter"){
-//       const newUser = e.target.value
-//       const newMessage = { type: 'postNotification', content: `${this.state.currentUser.name} has changed their name to ${newUser}`}
-//       this.sendMessage(newMessage)
-//       this.setState({currentUser: {name: newUser}})
-//     }
-//   }
-//
-//   handleChange(e) {
-//     if(e.key == "Enter"){
-//       e.preventDefault
-//       const newMessage ={ type: 'postMessage', username: this.state.currentUser.name, content: e.target.value }
-//       this.sendMessage(newMessage)
-//       e.target.value = ''
-//     }
-//   }
-//
-//   render() {
-//     console.log("Rendering <App/>");
-//     return (
-//       <div>
-//         <NavBar />
-//         <MessageList messages = {this.state.messages} />
-//         <ChatBar name = {this.state.currentUser.name} handleChange = {this.handleChange.bind(this)} changeUser = {this.changeUser.bind(this)} />
-//       </div>
-//     );
-//   }
-// }
-// export default App;
